03bfd15c2854451a1d1e6842602873b9c401e410
flatbetter <eyosiyas874@gmail.com>
2025-08-06 12:02:40 +0000

    Add files via upload

--- dino javascript.js
+++ dino javascript.js
@@ -2,5 +2,5 @@ const canvas = document.getElementById("cool");
 const ctx = canvas.getContext("2d");
 
 const cool_WIDTH = 800;
-const cool_HEIGHT = 200
-console.log("hello")
+const cool_HEIGHT = 200;
+console.log("hello");
